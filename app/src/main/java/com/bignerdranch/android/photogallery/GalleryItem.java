ede33704ce2e9b914dd10a616cfb85262040af67
Clifford-D <sir2cliff@hotmail.com>
2017-11-02 16:49:18 -0500

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/GalleryItem.java
@@ -0,0 +1,41 @@
+package com.bignerdranch.android.photogallery;
+
+/**
+ * Created by Dustin on 10/31/2017.
+ */
+
+public class GalleryItem {
+
+    public String getmCaption() {
+        return mCaption;
+    }
+
+    public void setmCaption(String mCaption) {
+        this.mCaption = mCaption;
+    }
+
+    public String getmId() {
+        return mId;
+    }
+
+    public void setmId(String mId) {
+        this.mId = mId;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+
+    private String mCaption;
+    private String mId;
+    private String mUrl;
+    @Override
+    public String toString() {
+        return mCaption;
+    }
+
+}
---
+++ app/src/main/java/com/bignerdranch/android/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,49 @@
+package com.bignerdranch.android.photogallery;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.UUID;
+
+/**
+ * Created by Dustin on 10/12/2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+//    public  static final String EXTRA_CRIME_ID =
+//            "com.bignerdranch.android.criminalintent.crime_id";
+//
+//    public static Intent newIntent(Context packageContext, UUID crimeId) {
+//        Intent intent = new Intent(packageContext, CrimeActivity.class);
+//        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+//        return intent;
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_fragment);
+        setContentView(getLayoutResId());
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+}
